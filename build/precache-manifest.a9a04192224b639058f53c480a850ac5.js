3564507ae82aed5c15e1b345313ef011bfa51fe1
Valtteri Luodemäki <valtteri.luodemaki@Valtteri-MBP.home>
2020-05-28 17:21:20 +0300

    puhelinluettelo full stack 3.11

---
+++ build/precache-manifest.a9a04192224b639058f53c480a850ac5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d2086604d95a1c4166aa34337a496b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6bb2b5a172763fbc32e",
+    "url": "/static/css/main.f7e7aee6.chunk.css"
+  },
+  {
+    "revision": "f5f7b13d34d14b56302e",
+    "url": "/static/js/2.076799b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.076799b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6bb2b5a172763fbc32e",
+    "url": "/static/js/main.c7c240de.chunk.js"
+  },
+  {
+    "revision": "d6d4691db26ca793a81a",
+    "url": "/static/js/runtime-main.4f3ee9ca.js"
+  }
+]);
