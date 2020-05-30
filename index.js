7b15e398a4d495deab9c749ca470fb52d6fefeca
Valtteri Luodemäki <valtteri.luodemaki@Valtteri-MBP.home>
2020-05-30 16:01:20 +0300

    puhelinluettelo ja tietokanta 3.16

--- index.js
+++ index.js
@@ -127,7 +127,6 @@ app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
-
   if (error.name === 'CastError' && error.kind == 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
   } 
