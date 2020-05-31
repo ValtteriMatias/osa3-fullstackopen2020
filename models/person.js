ef27584590a1fc9ced9d90e3fa848bbe62f05e49
Valtteri Luodemäki <valtteri.luodemaki@Valtteri-MBP.home>
2020-05-31 15:08:14 +0300

    puhelinluettelo ja tietokanta 3.20

--- models/person.js
+++ models/person.js
@@ -18,13 +18,13 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 const personSchema = new mongoose.Schema({
     name: {
       type: String,
-      minlength: 2,
+      minlength: 3,
       required: true,
       unique: true
     },
     number: {
       type: String,
-      minlength: 2,
+      minlength: 8,
       required: true
     }
 })
