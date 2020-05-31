82b35726c4a3d6e565bf09e280fc8eb330786102
Valtteri Luodemäki <valtteri.luodemaki@Valtteri-MBP.home>
2020-05-31 23:43:14 +0300

    lint-konfiguraatio 3.22 fix

--- mongo.js
+++ mongo.js
@@ -28,7 +28,7 @@ if (process.argv.length > 4) {
     number: process.argv[4],
   })
 
-  person.save().then(response => {
+  person.save().then(() => {
     console.log(`added ${person.name} number ${person.number} to phonebook`)
     mongoose.connection.close()
   })
