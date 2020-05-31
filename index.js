d041996292d2cb348967b35ea82d0a1b391e332b
Valtteri Luodemäki <valtteri.luodemaki@Valtteri-MBP.home>
2020-05-31 13:59:58 +0300

    puhelinluettelo ja tietokanta 3.19

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const generateId = () => {
   return Math.floor(Math.random() * (max - min)) + min
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
@@ -98,6 +98,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -145,7 +146,9 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if (error.name === 'CastError' && error.kind == 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
